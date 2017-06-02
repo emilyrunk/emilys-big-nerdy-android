59b7e2478d8dc8bc5e7a3096ba3f02a38edfad4c
Emily Runk <emily.runk01@gmail.com>
2017-06-01 17:30:51 -0700

    Chapter 8 Displaying Lists with RecyclerView

--- CriminalIntent/app/src/main/java/com/example/emilyrunk/criminalintent/CriminalListActivity.java
+++ CriminalIntent/app/src/main/java/com/example/emilyrunk/criminalintent/CriminalListActivity.java
@@ -2,10 +2,14 @@ package com.example.emilyrunk.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+/**
+ * Created by emilyrunk on 5/25/17.
+ */
+
+public class CriminalListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
