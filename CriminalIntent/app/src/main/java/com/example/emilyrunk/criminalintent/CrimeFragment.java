200ad75c38e27c7ff86133d5b8ac5f4f1ce56218
Emily Runk <emily.runk01@gmail.com>
2017-08-04 16:42:36 -0700

    Chapter 15 Implicit Intents Send Crime, Add Suspect to Crime object and database, ensure there's an app to handle setting suspect via Contacts app

--- CriminalIntent/app/src/main/java/com/example/emilyrunk/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/example/emilyrunk/criminalintent/CrimeFragment.java
@@ -6,12 +6,17 @@ package com.example.emilyrunk.criminalintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,11 +37,14 @@ public class CrimeFragment extends Fragment {
     private static final String DIALOG_DATE = "DialogDate";
 
     private static final int REQUEST_DATE = 0;
+    private static final int REQUEST_CONTACT = 1;
 
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private Button mSuspectButton;
+    private Button mReportButton;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -112,6 +120,46 @@ public class CrimeFragment extends Fragment {
             }
         });
 
+
+        mReportButton = (Button) v.findViewById(R.id.crime_report);
+        mReportButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Create an intent that sends a report of the crime via email?
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport()); //The body of the email
+                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject)); //The subject in the email
+                //createChooser() makes user choose an activity/app each time you send something
+                i.createChooser(i, getString(R.string.send_report));
+                startActivity(i);
+            }
+        });
+
+
+        //Using pickContent more than once so it will be outside
+        final Intent pickContact = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+
+        mSuspectButton = (Button) v.findViewById(R.id.crime_suspect);
+        mSuspectButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivityForResult(pickContact, REQUEST_CONTACT);
+            }
+        });
+        if (mCrime.getSuspect() != null) {
+            mSuspectButton.setText(mCrime.getSuspect());
+        }
+
+        //Check with PackageManager to see if device/user has contacts app
+        //PackageManager knows all components installed on device
+        //resolveActivity() find activity that matches the intent you gave it, MATCH_DEFAULT_ONLY flag
+        //restricts search to activities with the CATEGORY_DEFAULT flag, just like startActivity does.
+        //If search successful, return instance of ResolveInfo with activities found, if search returns null
+        // then no contacts app, then disable the useless button.
+        PackageManager packageManager = getActivity().getPackageManager();
+        if (packageManager.resolveActivity(pickContact, PackageManager.MATCH_DEFAULT_ONLY) == null) {
+            mSuspectButton.setEnabled(false);
+        }
+
         return v;
     }
 
@@ -124,10 +172,61 @@ public class CrimeFragment extends Fragment {
             Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
             mCrime.setDate(date);
             updateDate();
+        } else if (requestCode == REQUEST_CONTACT && data != null) {
+            Uri contactUri = data.getData();
+            //Specify which fields you want your query to return values for
+            String[] queryFields = new String[] {
+                    ContactsContract.Contacts.DISPLAY_NAME
+            };
+            //Perform your query - the contactUri is like a "where" clause here
+            Cursor c = getActivity().getContentResolver().query(contactUri, queryFields, null, null, null);
+
+            try {
+                //Double-check that you actually have results
+                if (c.getCount() == 0) {
+                    return;
+                }
+                //Pull out first column of first row of data - that is your suspect's name
+                c.moveToFirst();
+                String suspect = c.getString(0);
+                //read the suspect into our data/crime object
+                mCrime.setSuspect(suspect);
+                //set the suspect name as the text in the button
+                mSuspectButton.setText(suspect);
+            } finally {
+                c.close();
+            }
         }
     }
 
     private void updateDate() {
         mDateButton.setText(mCrime.getDate().toString());
+    }
+
+    //Format the string for the crime report (that will be sent)
+
+    private String getCrimeReport() {
+        String solvedString = null;
+
+        if (mCrime.isSolved()) {
+            solvedString = getString(R.string.crime_report_solved);
+        } else {
+            solvedString = getString(R.string.crime_report_unsolved);
+        }
+
+        String dateFormat = "EEE, MMM dd";
+        String dateString = DateFormat.format(dateFormat, mCrime.getDate()).toString();
+
+        String suspect = mCrime.getSuspect();
+        if (suspect == null) {
+            suspect = getString(R.string.crime_report_no_suspect);
+        } else {
+            suspect = getString(R.string.crime_report_suspect, suspect);
+        }
+
+        //1st param: string to format with other arguments to stick into spots within first param
+        String report = getString(R.string.crime_report, mCrime.getTitle(), dateString, solvedString, suspect);
+
+        return report;
     }
 }
